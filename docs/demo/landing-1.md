16915c039bc3930420d45912bc1954d3e3e23266
lichingchester <liching.chester@gmail.com>
2024-10-29 00:30:01 +0800

    update landing 1

--- docs/demo/landing-1.md
+++
@@ -1,9 +0,0 @@
----
-layout: Demo
----
-
-<!--@include: ../../src/landing/1/index.html-->
-
-<style>
-@import "../../src/landing/1/style.css";
-</style>
---
+++ src/components/TempDemoPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tcss--wrapper">
+    <div class="tcss--background">
+      <img src="https://assets.codepen.io/4926399/DSCF5648.jpg" alt="" />
+    </div>
+    <div class="tcss--front">
+      <div class="tcss--title"></div>
+      <div class="tcss--sub-title"></div>
+      <div class="tcss--sub-title"></div>
+    </div>
+  </div>
+</template>
+
+<style>
+.tcss__wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #fff;
+}
+
+.tcss__left {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  height: 100%;
+  background-color: #f5f5f5;
+}
+
+.tcss__right {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  height: 100%;
+  background-color: #f5f5f5;
+}
+</style>
